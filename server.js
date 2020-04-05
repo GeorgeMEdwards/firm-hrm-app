47cf7afb3a03f290db0435e6ee360e87aa4c4f22
GeorgeMEdwards <georgemwangi96@yahoo.com>
2020-04-05 17:25:13 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+require('./models/db');
+
+
+const employeeController = require('./controller/employeeController');
+const mongoose = require('mongoose');
+const Employee = mongoose.model('Employee');
+
+const express = require('express');
+
+const path = require('path');
+
+const bodyParser = require('body-parser');
+
+const expressHandlebars = require('express-handlebars');
+
+
+var app = express();
+
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+app.use(bodyParser.json());
+
+app.set('views',path.join(__dirname,'/views/'))
+
+app.engine('hbs',expressHandlebars({
+    extname:'hbs',
+    defaultLayout:'mainLayout',
+    layoutsDir:__dirname + '/views/layouts/'
+}))
+
+app.set('view engine','hbs');
+
+app.listen(5000,() => {
+    console.log("Server is listening on Port 5000");
+})
+
+app.use('/employee',employeeController)
+
+
+
